a392afe42eec5829a6ecf243221b2c601fb9a457
andron152d <Dmitr_jm76@student.itstep.org>
2026-09-23 18:26:52 +0300

    Fix workflow indentation and vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // <-- Добавьте эту строку
+  base: './',
   plugins: [react()],
   server: {
     port: 5173,
